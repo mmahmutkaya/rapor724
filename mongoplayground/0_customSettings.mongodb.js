0b5c285f7f76562c26e3125369769a8bb56a39e0
Mahmut Kaya <mmahmutkaya@gmail.com>
2025-07-30 20:05:55 +0500

    aa

--- mongoplayground/0_customSettings.mongodb.js
+++ mongoplayground/0_customSettings.mongodb.js
@@ -4,26 +4,26 @@ const customSettings = {
   pages: {
     firmapozlari: {
       basliklar: [
-        { id: "aciklama", baslikName: "Açıklama", visible: true, show: true },
-        { id: "versiyon", baslikName: "Versiyon", visible: true, show: true }
+        { id: "aciklama", baslikName: "Açıklama", visible: true, show: false },
+        { id: "versiyon", baslikName: "Versiyon", visible: true, show: false }
       ]
     },
     pozlar: {
       basliklar: [
-        { id: "aciklama", baslikName: "Açıklama", visible: true, show: true },
-        { id: "versiyon", baslikName: "Versiyon", visible: true, show: true }
+        { id: "aciklama", baslikName: "Açıklama", visible: true, show: false },
+        { id: "versiyon", baslikName: "Versiyon", visible: true, show: false }
       ]
     },
     mahaller: {
       basliklar: [
-        { id: "aciklama", baslikName: "Açıklama", visible: true, show: true },
-        { id: "versiyon", baslikName: "Versiyon", visible: true, show: true }
+        { id: "aciklama", baslikName: "Açıklama", visible: true, show: false },
+        { id: "versiyon", baslikName: "Versiyon", visible: true, show: false }
       ]
     },
     metrajpozlar: {
       basliklar: [
-        { id: "aciklama", baslikName: "Açıklama", visible: true, show: true },
-        { id: "versiyon", baslikName: "Versiyon", visible: true, show: true }
+        { id: "aciklama", baslikName: "Açıklama", visible: true, show: false },
+        { id: "versiyon", baslikName: "Versiyon", visible: true, show: false }
       ],
       showHasMahal: false
     }
