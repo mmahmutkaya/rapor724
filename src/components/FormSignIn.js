e5c4ab36d91a254d77faaeb015a437ec1c29994b
Mahmut Kaya <mmahmutkaya@gmail.com>
2026-01-08 00:08:31 +0300

    aa

--- src/components/FormSignIn.js
+++ src/components/FormSignIn.js
@@ -132,7 +132,7 @@ export default function SignIn() {
       const response = await fetch(`api/user/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        // body: JSON.stringify({ email, password })
+        body: JSON.stringify({ email, password })
       })
 
       const responseJson = await response.json()
