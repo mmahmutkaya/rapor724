aa67651f1e5658af6469a23523e2fc44157bde08
Mahmut Kaya <mmahmutkaya@gmail.com>
2026-02-11 01:10:46 +0300

    aa

--- src/hooks/useMongo.js
+++ src/hooks/useMongo.js
@@ -957,211 +957,211 @@ export const useGetProjectNames_firma = () => {
 // }
 
 
-export const useToggleOpenMetrajDugum = () => {
+// export const useToggleOpenMetrajDugum = () => {
 
-  // const RealmApp = useApp();
-  const queryClient = useQueryClient()
-  const { selectedProje, RealmApp } = useContext(StoreContext)
+//   // const RealmApp = useApp();
+//   const queryClient = useQueryClient()
+//   const { selectedProje, RealmApp } = useContext(StoreContext)
 
 
-  // const mahalListesi_optimisticUpdate = (mahalListesi, variables2) => {
+//   // const mahalListesi_optimisticUpdate = (mahalListesi, variables2) => {
 
-  //   const { _mahalId, _pozId, switchValue } = variables2
-  //   let list = mahalListesi.list
+//   //   const { _mahalId, _pozId, switchValue } = variables2
+//   //   let list = mahalListesi.list
 
 
-  //   if (switchValue) {
+//   //   if (switchValue) {
 
-  //     // openMetraj:false şeklinde property güncellemesi yapmıyorum çünkü backendden false olanlar dönmüyor, node sayıları backend den dönen hali aynı olsun diye
-  //     // backend ve frontend tutarlılığı için o sebeple listede olup olmadığına bakmıyorum, ekleyeceksek zaten yoktur, alt satırdaki filter onun için yoruma çevirildi
-  //     list = list.length ? [...list, { _mahalId, _pozId, openMetraj: switchValue }] : [{ _mahalId, _pozId, openMetraj: switchValue }]
-  //     // ilk seviye wbs'in yerleştirilmesi
+//   //     // openMetraj:false şeklinde property güncellemesi yapmıyorum çünkü backendden false olanlar dönmüyor, node sayıları backend den dönen hali aynı olsun diye
+//   //     // backend ve frontend tutarlılığı için o sebeple listede olup olmadığına bakmıyorum, ekleyeceksek zaten yoktur, alt satırdaki filter onun için yoruma çevirildi
+//   //     list = list.length ? [...list, { _mahalId, _pozId, openMetraj: switchValue }] : [{ _mahalId, _pozId, openMetraj: switchValue }]
+//   //     // ilk seviye wbs'in yerleştirilmesi
 
 
-  //     // mahallistesi.wbsLer den kontrol ederek, yukarı doğru giderek, listeden çıkarıyoruz
+//   //     // mahallistesi.wbsLer den kontrol ederek, yukarı doğru giderek, listeden çıkarıyoruz
 
-  //     // 
-  //     // let wbsCode2 = JSON.parse(JSON.stringify(wbsCode))
-  //     // let mapDurdur = false // mapDurdur true yapılırsa döngü daha da devam etmeden dursun diye
+//   //     // 
+//   //     // let wbsCode2 = JSON.parse(JSON.stringify(wbsCode))
+//   //     // let mapDurdur = false // mapDurdur true yapılırsa döngü daha da devam etmeden dursun diye
 
-  //     // esasen wbsCode2 döngüsü ile 
-  //     // wbsCode.split(".").map(c => {
+//   //     // esasen wbsCode2 döngüsü ile 
+//   //     // wbsCode.split(".").map(c => {
 
-  //     //   if (mapDurdur) {
-  //     //     return
-  //     //   }
+//   //     //   if (mapDurdur) {
+//   //     //     return
+//   //     //   }
 
-  //     //   // wbsLer boşsa herhangi bir sorgulama yapmadan yapıştır, aşağıda birdaha da boşmu diye sorgulama
-  //     //   if (!wbsLer) {
-  //     //     let { _id, code, name } = selectedProje.wbs.find(x => x.code === wbsCode2)
-  //     //     wbsLer = [{ _id, code, name }]
-  //     //   } else {
-  //     //     // wbsLer boş olsa bu sorgu hata verecek ama yukarıda yapıldı o sorgu, burdaysak doludur
+//   //     //   // wbsLer boşsa herhangi bir sorgulama yapmadan yapıştır, aşağıda birdaha da boşmu diye sorgulama
+//   //     //   if (!wbsLer) {
+//   //     //     let { _id, code, name } = selectedProje.wbs.find(x => x.code === wbsCode2)
+//   //     //     wbsLer = [{ _id, code, name }]
+//   //     //   } else {
+//   //     //     // wbsLer boş olsa bu sorgu hata verecek ama yukarıda yapıldı o sorgu, burdaysak doludur
 
-  //     //     // döngüdeki wbs mevcutsa, üst wbs'ler de mevcut demektir
-  //     //     if (wbsLer.find(x => x.code == wbsCode2)) {
-  //     //       mapDurdur = true
-  //     //       return
-  //     //     } else {
-  //     //       let { _id, code, name } = selectedProje.wbs.find(x => x.code === wbsCode2)
-  //     //       wbsLer = [...wbsLer, { _id, code, name }]
-  //     //     }
-  //     //   }
+//   //     //     // döngüdeki wbs mevcutsa, üst wbs'ler de mevcut demektir
+//   //     //     if (wbsLer.find(x => x.code == wbsCode2)) {
+//   //     //       mapDurdur = true
+//   //     //       return
+//   //     //     } else {
+//   //     //       let { _id, code, name } = selectedProje.wbs.find(x => x.code === wbsCode2)
+//   //     //       wbsLer = [...wbsLer, { _id, code, name }]
+//   //     //     }
+//   //     //   }
 
-  //     //   // bir sonraki döngü için wbsCode2 güncelliyoruz, bir üst seviye wbs code'una dönüştürüyoruz
-  //     //   if (wbsCode2.split(".").length > 1) wbsCode2 = wbsCode2.slice(0, wbsCode2.lastIndexOf("."))
+//   //     //   // bir sonraki döngü için wbsCode2 güncelliyoruz, bir üst seviye wbs code'una dönüştürüyoruz
+//   //     //   if (wbsCode2.split(".").length > 1) wbsCode2 = wbsCode2.slice(0, wbsCode2.lastIndexOf("."))
 
-  //     // })
+//   //     // })
 
-  //   }
+//   //   }
 
 
 
 
-  //   // if (!switchValue) {
+//   //   // if (!switchValue) {
 
-  //   //   // list den düğümü çıkartıyoruz
-  //   //   list = list.filter(x => !(x._mahalId.toString() === _mahalId.toString() && x._pozId.toString() === _pozId.toString()))
+//   //   //   // list den düğümü çıkartıyoruz
+//   //   //   list = list.filter(x => !(x._mahalId.toString() === _mahalId.toString() && x._pozId.toString() === _pozId.toString()))
 
-  //   //   // mahallistesi.wbsLer den seviye olarak yukarı doğru giderek, kontrol ederek, listeden çıkarıyoruz 
-  //   //   let wbsCode2 = JSON.parse(JSON.stringify(wbsCode))
-  //   //   let mapDurdur = false // mapDurdur true yapılırsa döngü daha da devam etmeden dursun diye
+//   //   //   // mahallistesi.wbsLer den seviye olarak yukarı doğru giderek, kontrol ederek, listeden çıkarıyoruz 
+//   //   //   let wbsCode2 = JSON.parse(JSON.stringify(wbsCode))
+//   //   //   let mapDurdur = false // mapDurdur true yapılırsa döngü daha da devam etmeden dursun diye
 
-  //   //   wbsCode.split(".").map((c, index) => {
+//   //   //   wbsCode.split(".").map((c, index) => {
 
-  //   //     if (mapDurdur) {
-  //   //       return
-  //   //     }
+//   //   //     if (mapDurdur) {
+//   //   //       return
+//   //   //     }
 
-  //   //     try {
-  //   //       if (list.find(x => x.wbsCode.indexOf(wbsCode2) === 0)) {
-  //   //         // bu code varsa üst kodlar da vardır döngü iptal edelim
-  //   //         mapDurdur = true
-  //   //         return
-  //   //       } else {
-  //   //         wbsLer = wbsLer.filter(x => x.code != wbsCode2)
-  //   //       }
-  //   //     } catch (error) {
-  //   //     }
+//   //   //     try {
+//   //   //       if (list.find(x => x.wbsCode.indexOf(wbsCode2) === 0)) {
+//   //   //         // bu code varsa üst kodlar da vardır döngü iptal edelim
+//   //   //         mapDurdur = true
+//   //   //         return
+//   //   //       } else {
+//   //   //         wbsLer = wbsLer.filter(x => x.code != wbsCode2)
+//   //   //       }
+//   //   //     } catch (error) {
+//   //   //     }
 
-  //   //     // bir sonraki döngü için wbsCode2 güncelliyoruz, bir üst seviye wbs code'una dönüştürüyoruz, swicthValue true modundayken sonunda nokta yok, yapı farklı
-  //   //     if (wbsCode2.split(".").length > 1) wbsCode2 = wbsCode2.slice(0, wbsCode2.lastIndexOf("."))
+//   //   //     // bir sonraki döngü için wbsCode2 güncelliyoruz, bir üst seviye wbs code'una dönüştürüyoruz, swicthValue true modundayken sonunda nokta yok, yapı farklı
+//   //   //     if (wbsCode2.split(".").length > 1) wbsCode2 = wbsCode2.slice(0, wbsCode2.lastIndexOf("."))
 
-  //   //   })
+//   //   //   })
 
-  //   // }
+//   //   // }
 
-  //   // mahalListesi.list = list
-  //   // mahalListesi.wbsLer = wbsLer
-  //   // return mahalListesi
-  // }
+//   //   // mahalListesi.list = list
+//   //   // mahalListesi.wbsLer = wbsLer
+//   //   // return mahalListesi
+//   // }
 
 
-  return useMutation({
-    mutationFn: ({ _mahalId, _pozId, switchValue }) => {
-      return RealmApp?.currentUser.callFunction("collectionDugumler", ({ functionName: "toggle_openMetraj", _projectId: selectedProje?._id, _mahalId, _pozId, switchValue }))
-    },
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['mahalListesi', selectedProje?._id.toString()] })
-    // onSuccess: (returnData, variables2) => queryClient.setQueryData(['mahalListesi', selectedProje?._id.toString()], (mahallistesi) => mahalListesi_optimisticUpdate(mahallistesi, variables2))
-  })
+//   return useMutation({
+//     mutationFn: ({ _mahalId, _pozId, switchValue }) => {
+//       return RealmApp?.currentUser.callFunction("collectionDugumler", ({ functionName: "toggle_openMetraj", _projectId: selectedProje?._id, _mahalId, _pozId, switchValue }))
+//     },
+//     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['mahalListesi', selectedProje?._id.toString()] })
+//     // onSuccess: (returnData, variables2) => queryClient.setQueryData(['mahalListesi', selectedProje?._id.toString()], (mahallistesi) => mahalListesi_optimisticUpdate(mahallistesi, variables2))
+//   })
 
-}
+// }
 
 
 
 
 
-export const useUpdateHazirlananMetrajShort = () => {
+// export const useUpdateHazirlananMetrajShort = () => {
 
-  // const RealmApp = useApp();
-  const queryClient = useQueryClient()
-  const { selectedProje, RealmApp } = useContext(StoreContext)
+//   // const RealmApp = useApp();
+//   const queryClient = useQueryClient()
+//   const { selectedProje, RealmApp } = useContext(StoreContext)
 
-  const mahalListesi_optimisticUpdate = (mahalListesi, variables) => {
+//   const mahalListesi_optimisticUpdate = (mahalListesi, variables) => {
 
-    const { _mahalId, _pozId, metraj, switchValue } = variables
-    let list = mahalListesi.list
+//     const { _mahalId, _pozId, metraj, switchValue } = variables
+//     let list = mahalListesi.list
 
-    list = list.map(x => {
-      if (x._mahalId.toString() == _mahalId.toString() && x._pozId.toString() == _pozId.toString()) {
-        x.metraj = metraj
-      }
-      return x
-    })
+//     list = list.map(x => {
+//       if (x._mahalId.toString() == _mahalId.toString() && x._pozId.toString() == _pozId.toString()) {
+//         x.metraj = metraj
+//       }
+//       return x
+//     })
 
-    mahalListesi.list = list
-    return mahalListesi
-  }
+//     mahalListesi.list = list
+//     return mahalListesi
+//   }
 
 
-  return useMutation({
-    mutationFn: (mahalMetrajlar) => {
-      return RealmApp?.currentUser.callFunction("collectionDugumler", ({ functionName: "updateMahalMetraj", _projectId: selectedProje?._id, mahalMetrajlar }))
-    },
-    // onSuccess: () => queryClient.invalidateQueries({ queryKey: ["hazirlananMetraj"] })
-    // onSuccess: (returnData, variables) => queryClient.setQueryData(['mahalListesi', selectedProje?._id.toString()], (mahallistesi) => mahalListesi_optimisticUpdate(mahallistesi, variables))
-  })
+//   return useMutation({
+//     mutationFn: (mahalMetrajlar) => {
+//       return RealmApp?.currentUser.callFunction("collectionDugumler", ({ functionName: "updateMahalMetraj", _projectId: selectedProje?._id, mahalMetrajlar }))
+//     },
+//     // onSuccess: () => queryClient.invalidateQueries({ queryKey: ["hazirlananMetraj"] })
+//     // onSuccess: (returnData, variables) => queryClient.setQueryData(['mahalListesi', selectedProje?._id.toString()], (mahallistesi) => mahalListesi_optimisticUpdate(mahallistesi, variables))
+//   })
 
-}
+// }
 
 
 
 
 
 
-export const useUpdateHazirlananMetraj = () => {
+// export const useUpdateHazirlananMetraj = () => {
 
-  // const RealmApp = useApp();
-  const queryClient = useQueryClient()
-  const { selectedProje, RealmApp } = useContext(StoreContext)
+//   // const RealmApp = useApp();
+//   const queryClient = useQueryClient()
+//   const { selectedProje, RealmApp } = useContext(StoreContext)
 
-  const optimisticUpdate = (hazirlananMetrajlar, variables) => {
+//   const optimisticUpdate = (hazirlananMetrajlar, variables) => {
 
-    const { hazirlananMetraj_state } = variables
+//     const { hazirlananMetraj_state } = variables
 
-    hazirlananMetrajlar = hazirlananMetrajlar.filter(x => x._userId.toString() !== hazirlananMetraj_state._userId.toString())
+//     hazirlananMetrajlar = hazirlananMetrajlar.filter(x => x._userId.toString() !== hazirlananMetraj_state._userId.toString())
 
-    hazirlananMetrajlar = [...hazirlananMetrajlar, hazirlananMetraj_state]
+//     hazirlananMetrajlar = [...hazirlananMetrajlar, hazirlananMetraj_state]
 
-    return hazirlananMetrajlar
+//     return hazirlananMetrajlar
 
-  }
+//   }
 
-  return useMutation({
-    mutationFn: ({ selectedNode, hazirlananMetraj_state, setHazirlananMetraj_state }) => {
-      return RealmApp?.currentUser.callFunction("collectionDugumler", ({ functionName: "updateHazirlananMetraj", hazirlananMetraj_state, _projectId: selectedProje?._id, _mahalId: selectedNode?._mahalId, _pozId: selectedNode?._pozId }))
-    },
-    onSuccess: (returnData, variables) => {
-      queryClient.setQueryData(['hazirlananMetrajlar', variables.selectedNode._id.toString()], (hazirlananMetrajlar) => optimisticUpdate(hazirlananMetrajlar, variables))
-      variables.setHazirlananMetraj_state()
-    }
-  })
+//   return useMutation({
+//     mutationFn: ({ selectedNode, hazirlananMetraj_state, setHazirlananMetraj_state }) => {
+//       return RealmApp?.currentUser.callFunction("collectionDugumler", ({ functionName: "updateHazirlananMetraj", hazirlananMetraj_state, _projectId: selectedProje?._id, _mahalId: selectedNode?._mahalId, _pozId: selectedNode?._pozId }))
+//     },
+//     onSuccess: (returnData, variables) => {
+//       queryClient.setQueryData(['hazirlananMetrajlar', variables.selectedNode._id.toString()], (hazirlananMetrajlar) => optimisticUpdate(hazirlananMetrajlar, variables))
+//       variables.setHazirlananMetraj_state()
+//     }
+//   })
 
-}
+// }
 
 
 
 
 
 
-export const useUpdateOnaylananMetraj = () => {
+// export const useUpdateOnaylananMetraj = () => {
 
-  // const RealmApp = useApp();
-  const queryClient = useQueryClient()
-  const { selectedProje, RealmApp } = useContext(StoreContext)
+//   // const RealmApp = useApp();
+//   const queryClient = useQueryClient()
+//   const { selectedProje, RealmApp } = useContext(StoreContext)
 
-  return useMutation({
-    mutationFn: ({ selectedNode, hazirlananMetrajlar_state, setHazirlananMetrajlar_state, onaylananMetraj_state, setOnaylananMetraj_state }) => {
-      return RealmApp?.currentUser.callFunction("collectionDugumler", ({ functionName: "updateOnaylananMetraj", hazirlananMetrajlar_state, onaylananMetraj_state, _projectId: selectedProje?._id, _mahalId: selectedNode?._mahalId, _pozId: selectedNode?._pozId }))
-    },
-    onSuccess: (returnData, variables) => {
-      queryClient.setQueryData(['hazirlananMetrajlar', variables.selectedNode._id.toString()], variables.hazirlananMetrajlar_state)
-      queryClient.setQueryData(['onaylananMetraj', variables.selectedNode._id.toString()], variables.onaylananMetraj_state)
-      variables.setHazirlananMetrajlar_state()
-      variables.setOnaylananMetraj_state()
-    }
-  })
+//   return useMutation({
+//     mutationFn: ({ selectedNode, hazirlananMetrajlar_state, setHazirlananMetrajlar_state, onaylananMetraj_state, setOnaylananMetraj_state }) => {
+//       return RealmApp?.currentUser.callFunction("collectionDugumler", ({ functionName: "updateOnaylananMetraj", hazirlananMetrajlar_state, onaylananMetraj_state, _projectId: selectedProje?._id, _mahalId: selectedNode?._mahalId, _pozId: selectedNode?._pozId }))
+//     },
+//     onSuccess: (returnData, variables) => {
+//       queryClient.setQueryData(['hazirlananMetrajlar', variables.selectedNode._id.toString()], variables.hazirlananMetrajlar_state)
+//       queryClient.setQueryData(['onaylananMetraj', variables.selectedNode._id.toString()], variables.onaylananMetraj_state)
+//       variables.setHazirlananMetrajlar_state()
+//       variables.setOnaylananMetraj_state()
+//     }
+//   })
 
-}
+// }
 
 
